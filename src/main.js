cae8d38be37a6e94ed414066f6d90e998caaf4f1
Jarry007 <940615834@qq.com>
2021-05-15 12:03:50 +0800

    ï¼:sparkles: å¾html和pdf社å生成

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/style.css'
 import './assets/vivify.min.css'
+import {InputNumber,Icon,Button,Loading} from 'element-ui'
 
 Vue.config.productionTip = false
+
+Vue.use(InputNumber)
+Vue.use(Icon)
+Vue.use(Button)
+Vue.use(Loading.directive)
+
+Vue.prototype.$loading = Loading.service
+
 
 new Vue({
   render: h => h(App),
--- src/views/drag.vue
+++ src/views/drag.vue
@@ -27,7 +27,7 @@
           class="vivify popIn"
           :class="item.id === showSettingId ? 'cursor-move' : ''"
           style="position: absolute"
-          v-for="(item, index) in boardList"
+          v-for="item in boardList"
           :key="item.id"
           :w="main.w"
           :h="main.w / item.scale"
@@ -45,75 +45,84 @@
             canHandler
             @click.native.stop="showSetting(item)"
           >
-            <div
+            <!-- <div
               v-if="item.id === showSettingId"
               class="drag-close"
               @click="removeDrag(index)"
             >
               <i class="el-icon-delete"></i>
-            </div> </dragItem
+            </div>  -->
+            </dragItem
         ></VueDrag>
       </div>
     </div>
     <div class="rc">
+      <div class="foot">
+        <el-button
+          size="small"
+          type="danger"
+          @click="printContainerList"
+        >生成文档</el-button>
+        <el-button size="small" type="danger" @click="pdfContainerList"
+          >生成PDF</el-button
+        >
+        <el-button size="small" type="danger" @click="wordContainerList2"
+          >生成word2</el-button
+        >
+      </div>
+
       <h3>操作区宽高</h3>
       <div style="text-align: center">
-            宽<el-input-number
-              v-model="main.w"
-              controls-position="right"
-              :min="596"
-              :max="2000"
-              size="mini"
-              class="line"
-            ></el-input-number
-            >px
-          </div>
-          <div style="text-align: center; border-bottom: 1px solid #ddd">
-            高<el-input-number
-              v-model="main.h"
-              controls-position="right"
-              :min="100"
-              :max="2000"
-              size="mini"
-              class="line"
-            ></el-input-number
-            >px
-          </div>
+        宽<el-input-number
+          v-model="main.w"
+          controls-position="right"
+          :min="596"
+          :max="2000"
+          size="mini"
+          class="line"
+        ></el-input-number
+        >px
+      </div>
+      <div style="text-align: center; border-bottom: 1px solid #ddd">
+        高<el-input-number
+          v-model="main.h"
+          controls-position="right"
+          :min="100"
+          :max="2000"
+          size="mini"
+          class="line"
+        ></el-input-number
+        >px
+      </div>
 
-          <template v-if="showSettingId">
-            <h3><i class="el-icon-c-scale-to-original"></i> 元素调整</h3>
-            <div style="text-align: center">
-              字体大小<el-input-number
-                v-model="editBlock.fontSize"
-                controls-position="right"
-                @change="handleChange"
-                size="mini"
-                class="line"
-                :min="1"
-                :max="60"
-              ></el-input-number
-              >px
-            </div>
-            <div style="text-align: center; margin-bottom: 20px">
-              内容<el-input
-                v-model="editBlock.text"
-                style="width: 70%"
-                size="mini"
-                class="line"
-              ></el-input>
-            </div>
+      <template v-if="showSettingId">
+        <h3><i class="el-icon-c-scale-to-original"></i> 元素调整</h3>
+        <div style="text-align: center">
+          字体大小<el-input-number
+            v-model="editBlock.fontSize"
+            controls-position="right"
+            @change="handleChange"
+            size="mini"
+            class="line"
+            :min="1"
+            :max="60"
+          ></el-input-number
+          >px
+        </div>
+        <div style="text-align: center; margin-bottom: 20px">
+          内容<el-input
+            v-model="editBlock.text"
+            style="width: 70%"
+            size="mini"
+            class="line"
+          ></el-input>
+        </div>
 
-            <vs-button
-              danger
-              border
-              animation-type="vertical"
-              block
-              @click="removeDrag(editBlock, true)"
-            >
-              移出
-              <template #animate> <i class="el-icon-back"></i> </template
-            ></vs-button>
-          </template>
+        <el-button type="primary" @click="removeDrag(editBlock, true)">
+          移出
+          <template #animate> <i class="el-icon-back"></i> </template
+        ></el-button>
+      </template>
     </div>
   </div>
 </template>
@@ -121,9 +130,12 @@
 
 
 <script>
+import html2canvas from 'html2canvas'
+import { saveAs } from "file-saver";
 import { Img2Base } from "@/utils/util";
 import dragItem from "../components/dragItem";
 import VueDrag from "vue-drag-resize";
+import jsPDF from 'jspdf'
 export default {
   components: {
     dragItem,
@@ -133,7 +145,7 @@ export default {
     return {
       boardList: [],
       list: [],
-      editBlock:{},
+      editBlock: {},
       showSettingId: "",
       main: {
         w: 595,
@@ -150,9 +162,105 @@ export default {
     });
   },
   methods: {
-    showSetting(e){
-      this.showSettingId = e.id
-      this.editBlock = e
+    printContainerList(){
+      console.log('大苏打')
+      const loading = this.$loading({
+        lock:true,
+        text:'文件生成中..'
+      })
+      const html_ = new Blob([this.renderHtml()], { type: "text/html;charset=utf-8" }) 
+      saveAs(html_,'报告文档.html') 
+      loading.close()
+    },
+    pdfContainerList(){
+      const loading = this.$loading({
+        lock:true,
+        text:'文件生成中..'
+      })
+
+      const dom = this.$refs.mainContainer
+      html2canvas(dom).then(res=>{
+        const contenWidth = res.width
+        const contentHeight = res.height
+        
+        let position = 0;
+    
+        const pageSizeW = 595.28
+        const pageSizeH = 841.89
+
+        const pageH = (pageSizeW / contenWidth) * contentHeight 
+
+        let pageHeight = pageH
+        const pageData = res.toDataURL('image/jpeg')
+
+        loading.close()
+
+        const PDF = new jsPDF('','pt','a4')
+
+        if(pageH < pageSizeH) {
+          PDF.addImage(pageData,'JPEG',0,0,pageSizeW,pageH)
+        }else{
+          while(pageHeight > 0 ){
+            PDF.addImage(pageData,"JPEG",0,position,pageSizeW,pageH)
+
+            pageHeight -= pageSizeH
+            position -= pageSizeH
+
+            if(pageHeight > 0){
+              PDF.addPage()
+            }
+          }
+        }
+
+
+        PDF.save('报告.pdf')
+      })
+
+      // loading.close()
+    },
+    wordContainerList2(){
+
+    },
+    renderHtml(){
+      const dom = this.$refs.mainContainer.innerHTML;
+      const tempalte_ = `
+      <!DOCTYPE html>
+                <html>
+                <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+                    <title>报告文档</title>
+                </head>
+                <body>
+                    <div style="width:${this.main.w}px;height:${this.main.h}px;position: relative;background:#f3f6fb">
+                    ${dom}
+                    </div>
+                </body>
+                </html>
+      `;
+      return tempalte_
+    },
+    removeDrag(data_, id = false) {
+      if (id) {
+        const item = this.boardList.find((item) => item.id === data_.id);
+        const index = this.boardList.findIndex(({ id }) => id === data_.id);
+        this.boardList.splice(index, 1);
+        this.list.push(item);
+        this.clickContainer();
+      } else {
+        const item = this.boardList[data_];
+        this.boardList.splice(data_, 1);
+        this.list.push(item);
+        this.clickContainer();
+      }
+    },
+    // 取消图片的编辑状态
+    clickContainer() {
+      this.showSettingId = "";
+    },
+    showSetting(e) {
+      this.showSettingId = e.id;
+      this.editBlock = e;
     },
     initBoard() {
       const board_ = this.$refs["controlBoard"];
@@ -269,7 +377,7 @@ export default {
 }
 .main {
   margin: 0 auto;
-  background: darkturquoise;
+  background: #f4f4f4;
   position: relative;
 }
 .main .tips-text {
